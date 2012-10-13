e9ace22ee11eb5d767a37e5ede8d9deacde9e50c
kelnar.mar@gmail.com <kelnar.mar@gmail.com>
2012-10-13 08:30:05 +0000

    demo class and edit pom.xml and persistence.xml

--- languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/App.java
+++ languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/App.java
@@ -1,5 +1,9 @@
 package cz.muni.fi.pa165.languageschool;
 
+import cz.muni.fi.pa165.languageschool.DAO.StudentDAO;
+import cz.muni.fi.pa165.languageschool.DAO.StudentDAOImpl;
+import cz.muni.fi.pa165.languageschool.entities.Student;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		
+		
+		Student student = new Student();
+		student.setFirstName("Martin");
+		student.setLastName("Novak");
+		
+		StudentDAO students = new StudentDAOImpl();
+		students.create(student);
+		
     }
 }
