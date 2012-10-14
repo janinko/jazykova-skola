23f70e41f9f04ba23a05ab6da9a0461a3937b20f
kelnar.mar@gmail.com <kelnar.mar@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-10-14 15:30:04 +0000

    edit LectionDAO and CourseDAO

--- trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/DAO/LectionDAO.java
+++ trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/DAO/LectionDAO.java
@@ -9,11 +9,57 @@ import java.util.List;
  * @author xchrastk
  */
 public interface LectionDAO {
+	
+	/**
+	 * Save new lection. One course is composed of several lections.
+	 * 
+	 * @param lection which will be saved
+	 * @return saved lection
+	 * @throws IllegalArgumentException if lection is null
+	 */
     public Lection create(Lection lection);
+	
+	
+	/**
+	 * Returns lection by id.
+	 *
+	 * @param id is unique number of lection.
+	 * @return one lection with the id
+	 * @throws IllegalArgumentException id is null or is < 1
+	 */
     public Lection read(long id);
+	
+	/**
+	 * Change lection which was saved previously.
+	 * 
+	 * @param lection which will be changed
+	 * @return changed lection
+	 * @throws IllegalArgumentException if lection is null
+	 */
     public Lection update(Lection lection);
+	
+	/**
+	 * Delete of lection
+	 * 
+	 * @param lection which will be deleted
+	 * @return deleted lection
+	 * @throws IllegalArgumentException if lection is null
+	 */
     public Lection delete(Lection lection);
     
+	
+	/**
+	 * Return all lections.
+	 * 
+	 * @return List of all lections.
+	 */
     public List<Lection> findAllLections();
+	
+	/**
+	 * Return list of lections depending on course.
+	 * 
+	 * @param course for which lection will be returned
+	 * @return list of lections
+	 */
     public List<Lection> findLectionByCourse(Course course);
 }
--- trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/DAO/StudentDAO.java
+++ trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/DAO/StudentDAO.java
@@ -9,12 +9,67 @@ import java.util.List;
  * @author xchrastk
  */
 public interface StudentDAO {
-    public Student create(Student cours);
+	
+	/**
+	 * This method save new student.
+	 * 
+	 * @param new student
+	 * @return new student who is in database now
+	 * @throws IllegalArgumentException if student is null
+	 */
+    public Student create(Student student);
+	
+	/**
+	 * This method return student.
+	 * 
+	 * @param id unique number of student
+	 * @return Student by id
+	 * @throws IllegalArgumentException if there is id null or id <= 0
+	 */
     public Student read(long id);
+	
+	/**
+	 * This method change student who was saved previously
+	 * 
+	 * @param student new changed student
+	 * @return Student who was updated
+	 * @throws IllegalArgumentException if student is null
+	 */
     public Student update(Student student);
+	
+	
+	/**
+	 * This method delete student.
+	 * 
+	 * @param student who will be deleted.
+	 * @return deleted student
+	 * @throws IllegalArgumentException if student is null
+	 */
     public Student delete(Student student);
     
+	/**
+	 * This method returns all save students.
+	 * 
+	 * @return all students
+	 */
     public List<Student> findAllStudents();
+	
+	/**
+	 * This method returns students depending on combination first name and last name
+	 * 
+	 * @param firstName First name of student
+	 * @param lastName Last name of student
+	 * @return One or more students who have appropriate name
+	 * @throws IllegalArgumentException if firstName or lastName is null
+	 */
     public List<Student> findStudentByName(String firstName, String lastName);
+	
+	/**
+	 * This method return list of students for the lection.
+	 * 
+	 * @param lection which students have registered
+	 * @return one or more students
+	 * @throws IllegalArgumentException if lection is null
+	 */
     public List<Student> findStudentByLection(Lection lection);
 }
