a92c3851d2529e153dc6574e52612b06dae3a191
Martina Chrástková <zabacorporation@gmail.com>
2012-10-23 18:05:46 +0000

    Added concept of service interfaces

---
+++ languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/StudentService.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.pa165.languageschool.services;
+
+import cz.muni.fi.pa165.languageschool.services.*;
+import cz.muni.fi.pa165.languageschool.entities.Course;
+import cz.muni.fi.pa165.languageschool.entities.Lection;
+
+/**
+ *
+ * @author xchrastk
+ */
+public interface StudentService {
+    
+    
+    
+    //+ CRUD operace
+}
