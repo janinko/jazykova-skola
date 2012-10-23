90a0d020c21c426737d655faf298196c9f292974
zabacorporation@gmail.com <zabacorporation@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-10-23 18:05:46 +0000

    Added concept of service interfaces

---
+++ trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/TeacherService.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.languageschool.services;
+
+import cz.muni.fi.pa165.languageschool.entities.Teacher.Language;
+
+/**
+ *
+ * @author xchrastk
+ */
+public interface TeacherService {
+    void addLanguage(Language language);
+    
+    //+ CRUD operace
+}
