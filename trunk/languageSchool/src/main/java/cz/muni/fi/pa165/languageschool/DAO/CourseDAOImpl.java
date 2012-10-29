ff59ee8b316790d1af711a9416114a0e01f1f762
kelnar.mar@gmail.com <kelnar.mar@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-10-29 21:58:49 +0000

    part of spring implementation

--- trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/DAO/CourseDAOImpl.java
+++ trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/DAO/CourseDAOImpl.java
@@ -5,72 +5,45 @@ import cz.muni.fi.pa165.languageschool.entities.Teacher.Language;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author xchrastk
  */
+@Repository
 public class CourseDAOImpl implements CourseDAO {
 
-    private EntityManagerFactory emf;
+	@PersistenceContext
+    private EntityManager em;
 
-    public CourseDAOImpl(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
-
+	
     public Course create(Course course) {
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
         em.persist(course);
-        em.getTransaction().commit();
-        Course c = em.find(Course.class, course.getId());
-        em.close();
-
-        return c;
+		
+		return course;
     }
 
     public Course read(long id) {
-        EntityManager em = emf.createEntityManager();
-
         return em.find(Course.class, id);
     }
 
-    public Course update(Course course) {
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-        Course c = em.merge(course);
-        em.persist(c);
-        em.getTransaction().commit();
-        em.close();
-
-        return c;
+    public void update(Course course) {
+        em.merge(course);
     }
 
-    public Course delete(Course course) {
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-        Course c = em.find(Course.class, course.getId());
-        em.remove(c);
-        em.getTransaction().commit();
-        em.close();
-
-        return course;
+    public void delete(Course course) {
+        em.remove(em.find(Course.class, course.getId()));
     }
 
     public List<Course> findAllCourses() {
-        EntityManager em = emf.createEntityManager();
-
         Query query = em.createQuery("SELECT c FROM Course c");
         return query.getResultList();
     }
 
     public List<Course> findCourseByLanguage(Language language) {
-        EntityManager em = emf.createEntityManager();
-        
         Query query = em.createQuery("SELECT c FROM Course c "
                 + "WHERE c.language = :language");
         query.setParameter("language", language);
