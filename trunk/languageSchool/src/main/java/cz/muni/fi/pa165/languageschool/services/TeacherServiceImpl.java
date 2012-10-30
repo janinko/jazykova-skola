e4e014d78255c53f831dda22c1cdd8d3fdaf3ba8
zabacorporation@gmail.com <zabacorporation@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-10-30 17:09:40 +0000

    Fixed TeacherService

--- trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/TeacherServiceImpl.java
+++ trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/TeacherServiceImpl.java
@@ -10,43 +10,45 @@ import java.util.List;
  * @author xchrastk
  */
 public class TeacherServiceImpl implements TeacherService {
-
+    
     private TeacherDAO dao;
 
+    
     public void setDao(TeacherDAO dao) {
         this.dao = dao;
     }
-
-    public void addLanguageToTeacher(Language language, Long id) {
-        Teacher t = dao.read(id);                
+    
+    public void addLanguageToTeacher(Language language, Long id){
+        Teacher t = dao.read(id);
         t.getLanguages().add(language);
+        dao.update(t);
     }
-
-    public void create(Teacher teacher) {
+    
+    public void create(Teacher teacher){
         dao.create(teacher);
     }
-
-    public Teacher read(Long id) {
+    
+    public Teacher read(Long id){        
         return dao.read(id);
     }
-
-    public void update(Teacher teacher) {
+    
+    public void update(Teacher teacher){
         dao.update(teacher);
-    }
-
-    public void delete(Teacher teacher) {
+    }    
+    
+    public void delete(Teacher teacher){
         dao.delete(teacher);
     }
-
-    public List<Teacher> findAllTeachers() {
+    
+    public List<Teacher> findAllTeachers(){
         return dao.findAllTeachers();
     }
-
-    public List<Teacher> findTeacherByName(String firstName, String lastName) {
+    
+    public List<Teacher> findTeacherByName(String firstName, String lastName){
         return dao.findTeacherByName(firstName, lastName);
     }
-
-    public List<Teacher> findTeacherByLanguage(Language language) {
+    
+    public List<Teacher> findTeacherByLanguage(Language language){
         return dao.findTeacherByLanguage(language);
-    }
+    } 
 }
