8b64ac0aa3082db39973106a3f4a77b41c69cae3
Honza Brázdil <janinko@users.noreply.github.com>
2012-10-30 02:26:26 +0000

    Some hibernated spring work

--- languageSchool/src/test/java/cz/muni/fi/pa165/languageschool/DAO/LessonDAOTest.java
+++ languageSchool/src/test/java/cz/muni/fi/pa165/languageschool/DAO/LessonDAOTest.java
@@ -1,42 +1,42 @@
 package cz.muni.fi.pa165.languageschool.DAO;
 
+import cz.muni.fi.pa165.languageschool.AbstractSpringTest;
 import cz.muni.fi.pa165.languageschool.entities.Course;
 import cz.muni.fi.pa165.languageschool.entities.Lesson;
 import java.util.List;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  * @author fivekeyem
  */
-public class LessonDAOTest {
+public class LessonDAOTest extends AbstractSpringTest{
 
-    private LessonDAO lessons;
-    //private CourseDAO courses;
+	@Autowired
+    private LessonDAO lessonDao;
+	
+	@Autowired
+    private CourseDAO courseDao;
 
     public LessonDAOTest() {
     }
 
     @Before
     public void setUp() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("lsPU");
-        lessons = new LessonDAOImpl(emf);
-        //courses = new CourseDAOImpl(emf);
     }
 
     @Test
     public void testCreate1() {
         Lesson l1 = new Lesson();
-        Lesson l2 = lessons.create(l1);
+        Lesson l2 = lessonDao.create(l1);
 
         assertEquals(l1, l2);
         assertEquals(l1.getId(), l2.getId());
 
-        lessons.delete(l1);
+        lessonDao.delete(l1);
     }
 
     @Test
@@ -45,44 +45,43 @@ public class LessonDAOTest {
 
         assertNull(l1.getId());
 
-        Lesson l2 = lessons.create(l1);
+        Lesson l2 = lessonDao.create(l1);
 
         assertNotNull(l1.getId());
         assertNotNull(l2.getId());
 
-        lessons.delete(l1);
+        lessonDao.delete(l1);
     }
 
     @Test
     public void testRead() {
         Lesson l1 = new Lesson();
 
-        lessons.create(l1);
+        lessonDao.create(l1);
 
-        Lesson l2 = lessons.read(l1.getId());
+        Lesson l2 = lessonDao.read(l1.getId());
 
         assertEquals(l1.getId(), l2.getId());
 
-        lessons.delete(l1);
+        lessonDao.delete(l1);
     }
 
     @Test
     public void testFindAllLessons() {
         Lesson l1 = new Lesson();
 
-        lessons.create(l1);
+        lessonDao.create(l1);
 
-        List<Lesson> ls = lessons.findAllLessons();
+        List<Lesson> ls = lessonDao.findAllLessons();
         Lesson l2 = ls.get(0);
 
         assertFalse(ls.isEmpty());
         assertEquals(l1.getId(), l2.getId());
 
-        lessons.delete(l1);
+        lessonDao.delete(l1);
     }
 
 	
-	/*
     @Test
     public void testFindLessonByCourse() {
         Lesson l1 = new Lesson();
@@ -94,25 +93,24 @@ public class LessonDAOTest {
         l2.setCourse(c2);
         l3.setCourse(c1);
 
-        courses.create(c1);
-        courses.create(c2);
-        lessons.create(l1);
-        lessons.create(l2);
-        lessons.create(l3);
+        courseDao.create(c1);
+        courseDao.create(c2);
+        lessonDao.create(l1);
+        lessonDao.create(l2);
+        lessonDao.create(l3);
 
-        List<Lesson> list = lessons.findLessonByCourse(c1);
+        List<Lesson> list = lessonDao.findLessonByCourse(c1);
 
         assertTrue(list.contains(l1));
         assertTrue(list.contains(l3));
         assertFalse(list.contains(l2));
         
-        lessons.delete(l1);
-        lessons.delete(l2);
-        lessons.delete(l3);
-        courses.delete(c1);
-        courses.delete(c2);
+        lessonDao.delete(l1);
+        lessonDao.delete(l2);
+        lessonDao.delete(l3);
+        courseDao.delete(c1);
+        courseDao.delete(c2);
     }
-	*/
 
     @Test
     public void testCreateAndDelete() {
@@ -120,22 +118,22 @@ public class LessonDAOTest {
         Lesson l2 = new Lesson();
         Lesson l3 = new Lesson();
 
-        lessons.create(l1);
-        lessons.create(l2);
-        lessons.create(l3);
+        lessonDao.create(l1);
+        lessonDao.create(l2);
+        lessonDao.create(l3);
 
-        List<Lesson> ls = lessons.findAllLessons();
+        List<Lesson> ls = lessonDao.findAllLessons();
 
         assertFalse(ls.isEmpty());
         assertTrue(ls.size() == 3);
 
         Lesson l4 = ls.get(1);
-        lessons.delete(l4);
+        lessonDao.delete(l4);
 
-        ls = lessons.findAllLessons();
+        ls = lessonDao.findAllLessons();
         assertTrue(ls.size() == 2);
         
-        lessons.delete(l1);
-        lessons.delete(l3);
+        lessonDao.delete(l1);
+        lessonDao.delete(l3);
     }
 }
