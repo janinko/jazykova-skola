67e17fe10ef543590c7a055dab7f532bc2e4c842
zabacorporation@gmail.com <zabacorporation@gmail.com>
2012-10-30 16:50:12 +0000

    TeacherService implementation

--- languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/TeacherService.java
+++ languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/TeacherService.java
@@ -1,13 +1,39 @@
 package cz.muni.fi.pa165.languageschool.services;
 
+import cz.muni.fi.pa165.languageschool.entities.Teacher;
 import cz.muni.fi.pa165.languageschool.entities.Teacher.Language;
+import java.util.List;
 
 /**
  *
  * @author xchrastk
  */
 public interface TeacherService {
-    void addLanguage(Language language);
+    /*
+     * Adds language to a teacher.
+     * @param language language to be added to the teacher
+     * @param id id of the teacher
+     */
+    void addLanguageToTeacher(Language language, Long id);
     
-    //+ CRUD operace
+    /*
+     * Adds a new teacher to the database. The teacher must not be null.
+     * 
+     * @param teacher teacher to be added
+     * @return teacher the new teacher (retrieved from the database)
+     * @throws IllegalArgumentException if the teacher is null
+     */   
+    void create(Teacher teacher);
+    
+    Teacher read(Long id);
+    
+    void update(Teacher teacher);    
+    
+    void delete(Teacher teacher);
+    
+    public List<Teacher> findAllTeachers();
+    
+    public List<Teacher> findTeacherByName(String firstName, String lastName);
+    
+    public List<Teacher> findTeacherByLanguage(Language language);    
 }
