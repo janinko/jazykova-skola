5b0b0b8011a8753402c74dd1239104833dd10ec4
Honza Brázdil <janinko.g@gmail.com>
2012-11-01 22:47:09 +0000

    SVN sucks. Actually add the CourseServiceTest.

---
+++ languageSchool/src/test/java/cz/muni/fi/pa165/languageschool/services/CourseServiceTest.java
@@ -0,0 +1,57 @@
+package cz.muni.fi.pa165.languageschool.services;
+
+import cz.muni.fi.pa165.languageschool.entities.Course;
+import cz.muni.fi.pa165.languageschool.entities.Teacher;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jbrazdil
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class CourseServiceTest {
+	
+	@Autowired
+	private LessonService lessonService;
+	
+	@Autowired
+	private CourseService courseService;
+	
+	
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testCreateCourse() {
+		Course c1 = new Course("Anglictina pro mirne zacatecniky");
+		c1.setLanguage(Teacher.Language.AJ);
+		c1.setLevel(2);
+		
+		courseService.createCourse(c1);
+		assertNotNull(c1.getId());
+		
+		Set<Course> courses = courseService.getAllCourses();
+		
+		assertTrue(courses.size() == 1);
+		
+		Course c2 = courses.iterator().next();
+		
+		assertEquals(c1.getId(), c2.getId());
+		assertEquals(c1.getLanguage(), c2.getLanguage());
+		assertEquals(c1.getLevel(), c2.getLevel());
+		assertEquals(c1.getName(), c2.getName());
+		assertEquals(c1, c2);
+		
+		courseService.deleteCourse(c2);
+	}
+	
+}
