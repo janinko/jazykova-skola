9d34995e479e85635660152ee2456ac9f89ce047
xkelnar <xkelnar@users.noreply.github.com>
2012-11-01 18:59:46 +0000

    StudentService in

--- languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/StudentService.java
+++ languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/StudentService.java
@@ -3,14 +3,23 @@ package cz.muni.fi.pa165.languageschool.services;
 import cz.muni.fi.pa165.languageschool.services.*;
 import cz.muni.fi.pa165.languageschool.entities.Course;
 import cz.muni.fi.pa165.languageschool.entities.Lesson;
+import cz.muni.fi.pa165.languageschool.entities.Student;
+import java.util.List;
 
 /**
  *
- * @author xchrastk
+ * @author fivekeyem
  */
 public interface StudentService {
     
-    
-    
-    //+ CRUD operace
+    public void update(Student student);
+	
+	public Student read(String email);
+	
+	public List<Lesson> getAllLections(Student student);
+	
+	public void lectionEnroll(Student student, Lesson lesson);
+	
+	public void lectionCancel(Student student, Lesson lesson);
+	
 }
