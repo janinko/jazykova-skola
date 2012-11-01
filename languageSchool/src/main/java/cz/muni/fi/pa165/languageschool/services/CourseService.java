701dd13b022d69515a54efe77460318f49a11310
Martina Chrástková <zabacorporation@gmail.com>
2012-11-01 19:10:06 +0000

    Course, Lesson,Teacher Service Interfaces

--- languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/CourseService.java
+++ languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/CourseService.java
@@ -2,16 +2,22 @@ package cz.muni.fi.pa165.languageschool.services;
 
 import cz.muni.fi.pa165.languageschool.entities.Course;
 import cz.muni.fi.pa165.languageschool.entities.Lesson;
+import cz.muni.fi.pa165.languageschool.entities.Teacher.Language;
+import java.util.Set;
 
 /**
  *
  * @author xchrastk
  */
 public interface CourseService {
-    /*
-     * @return updated lection
-     */
-    Course addLection(Lesson lesson);
     
-    //+ CRUD operace
+    void createCourse(Course course);
+    
+    void deleteCourse(Course course);
+    
+    void addLessonToCourse(Course course, Lesson lesson);   
+    
+    Set<Course> getCoursesByLanguage(Language language);
+    
+    Set<Course> getAllCourses();
 }
--- languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/TeacherService.java
+++ languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/TeacherService.java
@@ -1,39 +1,27 @@
 package cz.muni.fi.pa165.languageschool.services;
 
+import cz.muni.fi.pa165.languageschool.entities.Course;
+import cz.muni.fi.pa165.languageschool.entities.Lesson;
 import cz.muni.fi.pa165.languageschool.entities.Teacher;
-import cz.muni.fi.pa165.languageschool.entities.Teacher.Language;
-import java.util.List;
+import java.util.Set;
 
 /**
  *
  * @author xchrastk
  */
 public interface TeacherService {
-    /*
-     * Adds language to a teacher.
-     * @param language language to be added to the teacher
-     * @param id id of the teacher
-     */
-    void addLanguageToTeacher(Language language, Long id);
     
-    /*
-     * Adds a new teacher to the database. The teacher must not be null.
-     * 
-     * @param teacher teacher to be added
-     * @return teacher the new teacher (retrieved from the database)
-     * @throws IllegalArgumentException if the teacher is null
-     */   
-    void create(Teacher teacher);
+    void createTeacher(Teacher teacher);
     
-    Teacher read(Long id);
+    Teacher readTeacher(String email);
     
-    void update(Teacher teacher);    
+    void updateTeacher(Teacher teacher);    
     
-    void delete(Teacher teacher);
+    void deleteTeacher(Teacher teacher);
     
-    public List<Teacher> findAllTeachers();
+    Set<Lesson> getTeachersLessons(Teacher teacher);
     
-    public List<Teacher> findTeacherByName(String firstName, String lastName);
+    Set<Course> getTeachersCourses(Teacher teacher);
     
-    public List<Teacher> findTeacherByLanguage(Language language);    
+    Set<Teacher> getAllTeachers();
 }
