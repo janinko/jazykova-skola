60ce163dc0d8c3f2d006ae4bec29f68664918411
kelnar.mar@gmail.com <kelnar.mar@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-11-03 15:18:06 +0000

    rename transfer package

---
+++ trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/dto/StudentDto.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.languageschool.dto;
+
+import cz.muni.fi.pa165.languageschool.entities.Student;
+
+/**
+ *
+ * @author jbrazdil
+ */
+public class StudentDto {
+
+	public StudentDto(Student s) {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	public Long getId() {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	public int getAge() {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+}
