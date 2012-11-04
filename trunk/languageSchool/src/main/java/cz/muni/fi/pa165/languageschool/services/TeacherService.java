c43e60cf27ab39963b0b4db396976fff8aa1b51e
zabacorporation@gmail.com <zabacorporation@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-11-04 22:57:02 +0000

    TeacherServiceTest, javadoc

--- trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/TeacherService.java
+++ trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/services/TeacherService.java
@@ -10,18 +10,47 @@ import java.util.Set;
  * @author xchrastk
  */
 public interface TeacherService {
-    
+    /*
+     * Create teacher.
+     * @param teacher teacher to be created
+     */
     void createTeacher(Teacher teacher);
     
+    /*
+     * Read teacher with given email.
+     * @param email
+     * @return teacher teacher who has the email
+     */
     Teacher readTeacher(String email);
     
+    /*
+     * Updates the teacher.
+     * @param teacher updated teacher
+     */
     void updateTeacher(Teacher teacher);    
     
+    /*
+     * Deletes the teacher.
+     */
     void deleteTeacher(Teacher teacher);
     
+    /*
+     * Returns all teachers from the school.
+     * @return all teachers
+     */
+    Set<Teacher> getAllTeachers();
+    
+     /*
+     * Finds all lessons the teacher teaches.
+     * @param teacher
+     * @return lessons the teacher teaches
+     */
     Set<Lesson> getTeachersLessons(Teacher teacher);
     
+    /*
+     * Finds all courses the teacher teaches.
+     * @param teacher
+     * @return courses the teacher teaches
+     */
     Set<Course> getTeachersCourses(Teacher teacher);
-    
-    Set<Teacher> getAllTeachers();
 }
