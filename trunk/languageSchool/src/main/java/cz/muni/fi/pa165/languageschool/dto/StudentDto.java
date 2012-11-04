d6499a7a2381f8857d45b8b81845ee067ae91924
kelnar.mar@gmail.com <kelnar.mar@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-11-04 21:52:51 +0000

    Student DTO

--- trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/dto/StudentDto.java
+++ trunk/languageSchool/src/main/java/cz/muni/fi/pa165/languageschool/dto/StudentDto.java
@@ -8,20 +8,63 @@ import cz.muni.fi.pa165.languageschool.entities.Student;
 
 /**
  *
- * @author jbrazdil
+ * @author fivekeym
  */
 public class StudentDto {
+	
+	private String id;
+	private String firstName;
+	private String lastName;
+	private int age;
 
 	public StudentDto(Student s) {
-		throw new UnsupportedOperationException("Not yet implemented");
+		this.id = s.getId().toString();
+		this.firstName = s.getFirstName();
+		this.lastName = s.getLastName();
+		this.age = s.getAge();
 	}
 
-	public Long getId() {
-		throw new UnsupportedOperationException("Not yet implemented");
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public int getAge() {
-		throw new UnsupportedOperationException("Not yet implemented");
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
+	public Student adaptToEntity() {
+		Student s = new Student();
+		s.setId(Long.parseLong(getId()));
+		s.setFirstName(getFirstName());
+		s.setLastName(getLastName());
+		s.setAge(getAge());
+		
+		return s;
 	}
 	
 }
