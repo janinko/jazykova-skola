cd79a07bbbea967565ad6ac64f8e717e30bfece5
janinko.g@gmail.com <janinko.g@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-11-11 21:08:02 +0000

    set Generation Type to TABLE

--- branches/[ejb]/LanguageSchoolEJB/LanguageSchoolEJB-ejb/src/main/java/cz/muni/fi/pa165/languageschool/entities/Student.java
+++ branches/[ejb]/LanguageSchoolEJB/LanguageSchoolEJB-ejb/src/main/java/cz/muni/fi/pa165/languageschool/entities/Student.java
@@ -13,7 +13,7 @@ public class Student implements Serializable {
     private static final long serialVersionUID = 1L;
 	
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
     
     @Column(nullable=false, length=20)
