5a8ec8ecb3c189298e3370e2d5c154fbebc8696f
zabacorporation@gmail.com <zabacorporation@gmail.com>
2012-11-11 18:06:08 +0000

    Service beans

--- LanguageSchoolEJB/LanguageSchoolEJB-ejb/src/main/java/cz/muni/fi/pa165/languageschool/services/StudentService.java
+++ LanguageSchoolEJB/LanguageSchoolEJB-ejb/src/main/java/cz/muni/fi/pa165/languageschool/services/StudentService.java
@@ -1,12 +1,29 @@
 package cz.muni.fi.pa165.languageschool.services;
 
+import cz.muni.fi.pa165.languageschool.DAO.StudentDAOLocal;
+import cz.muni.fi.pa165.languageschool.entities.Student;
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.ejb.SessionContext;
+
 /**
  *
  * @author xchrastk
  */
 public class StudentService implements StudentServiceLocal {
+    @Resource
+    private SessionContext ctx;
     
+    @EJB
+    private StudentDAOLocal studentDao;
+
+    @Override
+    public void createStudent(Student student) {
+        studentDao.create(student);
+    }
     
-    
-    //+ CRUD operace
+    @Override
+    public void readStudent(Student student) {
+        studentDao.read(student.getId());
+    }
 }
