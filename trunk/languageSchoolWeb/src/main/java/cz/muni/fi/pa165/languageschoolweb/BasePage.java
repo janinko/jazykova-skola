3127acef5771618e9aaa6b681fcf1a3934614b89
kelnar.mar@gmail.com <kelnar.mar@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-11-21 19:38:26 +0000

    basic skeleton of html layout

---
+++ trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/BasePage.java
@@ -0,0 +1,25 @@
+/*
+ * WicketExamplePage.java
+ *
+ * Created on 21. listopad 2012, 20:21
+ */
+ 
+package cz.muni.fi.pa165.languageschoolweb;           
+
+import org.apache.wicket.markup.html.WebPage;
+
+/** 
+ *
+ * @author kelnar
+ * @version 
+ */
+
+public abstract class BasePage extends WebPage {
+
+    public BasePage() { 
+        super(); 
+        add(new HeaderPanel("headerpanel", "Welcome To Wicket")); 
+        add(new FooterPanel("footerpanel", "Powered by Wicket and the NetBeans Wicket Plugin"));
+    } 
+
+}
--- trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/HomePage.java
+++ trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/HomePage.java
@@ -1,18 +1,17 @@
-package cz.muni.fi.pa165.languageschoolweb;
+/*
+ * HomePage.java
+ *
+ * Created on 21. listopad 2012, 20:21
+ */
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
+package cz.muni.fi.pa165.languageschoolweb;           
+
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.WebPage;
 
-public class HomePage extends WebPage {
-	private static final long serialVersionUID = 1L;
+public class HomePage extends BasePage {
 
-    public HomePage(final PageParameters parameters) {
-	super(parameters);
-
-	add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-        // TODO Add your page's components here
-
+    public HomePage() {
+        add(new Label("message", "Hello, World!"));
     }
+
 }
