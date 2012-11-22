921c53eb7818957c3df5ec935543d9bec96d46d7
zabacorporation@gmail.com <zabacorporation@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-11-22 16:31:23 +0000

    Lookup in test

--- trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/HomePage.java
+++ trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/HomePage.java
@@ -6,12 +6,58 @@
 
 package cz.muni.fi.pa165.languageschoolweb;           
 
+import cz.muni.fi.pa165.languageschool.api.dto.CourseDto;
+import cz.muni.fi.pa165.languageschool.api.dto.LessonDto;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.AbstractItem;
+import org.apache.wicket.markup.repeater.RepeatingView;
 
 public class HomePage extends BasePage {
 
     public HomePage() {
-        add(new Label("message", "O nás!"));
+        CourseDto course1 = new CourseDto();
+        course1.setName("Fránina");
+        LessonDto lesson1 = new LessonDto();
+        lesson1.setCourse(course1);
+        lesson1.setTeacherName("Učák učitel");
+        Calendar date = (Calendar.getInstance());
+        date.set(2012,12,12);
+        lesson1.setDate(date);        
+        List<LessonDto> list = new ArrayList<LessonDto>();
+        list.add(lesson1);
+        CourseDto course2 = new CourseDto();
+        course2.setName("Ájina");
+        LessonDto lesson2 = new LessonDto();
+        lesson2.setCourse(course2);
+        lesson2.setTeacherName("Učák učitel");
+        date = (Calendar.getInstance());
+        date.set(2022,22,22);
+        lesson2.setDate(date);           
+        list.add(lesson2);
+        
+        RepeatingView repeating = new RepeatingView("repeating");
+        add(repeating);
+        
+        for(LessonDto lesson: list) {
+            AbstractItem item = new AbstractItem(repeating.newChildId());
+            
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM");            
+            DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+
+            item.add(new Label("date",dateFormat.format(lesson1.getDate().getTime()))) ;
+            item.add(new Label("time",timeFormat.format(lesson1.getDate().getTime()))) ;
+            item.add(new Label("name",lesson1.getCourse().getName())) ;
+            item.add(new Label("teacher", lesson1.getTeacherName())) ;
+            
+            repeating.add(item);
+        }
+        
+        
     }
 
 }
