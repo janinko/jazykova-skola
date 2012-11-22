28566c0db83bfbda376753b6164375e88fefdf75
Honza Brázdil <janinko.g@gmail.com>
2012-11-22 15:42:51 +0000

    bind spring to web

--- languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/WicketApplication.java
+++ languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/WicketApplication.java
@@ -2,6 +2,7 @@ package cz.muni.fi.pa165.languageschoolweb;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
@@ -27,6 +28,8 @@ public class WicketApplication extends WebApplication
 	{
 		super.init();
 
+		getComponentInstantiationListeners().add(new SpringComponentInjector(this));
+		
 		// add your configuration here
 	}
 }
