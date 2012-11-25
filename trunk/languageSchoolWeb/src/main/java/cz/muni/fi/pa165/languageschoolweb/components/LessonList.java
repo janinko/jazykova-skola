f2954e564d9af4addcfbd91f7cb7b516a866f439
zabacorporation@gmail.com <zabacorporation@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-11-25 22:12:55 +0000

    LessonsPage moved to LessonsList

--- trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/components/LessonList.java
+++ trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/components/LessonList.java
@@ -118,3 +118,93 @@ public class LessonList extends Panel{
 		}
 	}
 }
+
+/*
+ * public class LessonsList extends BasePage {
+    @SpringBean
+    private LessonDtoAdapter lessons;
+    @SpringBean
+    private CourseDtoAdapter courses;
+    @SpringBean
+    private GenerateDataService generator;
+
+    public LessonsList() {
+        generator.generateData(2, 3, 3, 2);
+        final Set<LessonDto> set = lessons.getAllLessons();
+
+        final Form form = new Form("form") {
+            protected void onSubmit() {
+                info("Form.onSubmit executed");
+            }
+        };
+
+
+        RepeatingView repeating = new RepeatingView("repeating");
+        add(repeating);
+        
+        final Map<CheckBox, LessonDto> deletes = new HashMap<CheckBox, LessonDto>();
+        final List<CheckBoxModel> deleteModels = new ArrayList<CheckBoxModel>();
+        
+        int i = 0;
+        for (LessonDto lesson : set) {
+            AbstractItem item = new AbstractItem(repeating.newChildId());
+
+            CheckBoxModel model = new CheckBoxModel(false);
+            deleteModels.add(model);
+            CheckBox check = new CheckBox("check", new PropertyModel<Boolean>(deleteModels.get(i),"checked"));
+            deletes.put(check, lesson);
+            i++;
+
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM");
+            DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+
+            item.add(new Label("date", dateFormat.format(lesson.getDate().getTime())));
+            item.add(new Label("time", timeFormat.format(lesson.getDate().getTime())));
+            item.add(new Label("name", lesson.getCourse().getName()));
+            item.add(new Label("teacher", lesson.getTeacherName()));
+            item.add(check);
+
+            repeating.add(item);
+        }
+
+        form.add(repeating);
+        Button button = new Button("sign") {
+            public void onSubmit() {                
+                boolean deleted = false;
+                for (CheckBox check : deletes.keySet()) {
+                    if (check.getModelObject() == null) {
+                        ;
+                    } else 
+                        if (check.getModelObject() == true) {                        
+                        lessons.removeLesson(deletes.get(check));
+                        set.remove(deletes.get(check));
+                        deleted = true;
+                    }
+                }
+                
+                throw new RestartResponseException(this.getPage());
+                
+            }
+        };
+        
+        form.add(button); 
+        addOrReplace(form);
+    }
+}
+
+class CheckBoxModel implements Serializable
+{private static final long serialVersionUID = 1L;
+  private boolean checked;
+
+  CheckBoxModel(Boolean checked)
+  {
+    this.checked = checked;
+  }
+
+  boolean isChecked()
+  {
+    return checked;
+  }
+}
+
+ */
