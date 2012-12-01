10fd14e14a268d0367d5e68d3a128a0d258dd120
janinko.g@gmail.com <janinko.g@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-12-01 20:11:42 +0000

    localization and cleanup

--- trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/components/AddCourseForm.java
+++ trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/components/AddCourseForm.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 kelnar.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package cz.muni.fi.pa165.languageschoolweb.components;
 
 import cz.muni.fi.pa165.languageschool.api.adapters.CourseDtoAdapter;
@@ -21,7 +6,6 @@ import cz.muni.fi.pa165.languageschool.api.entities.Teacher;
 import cz.muni.fi.pa165.languageschoolweb.CoursesPage;
 import cz.muni.fi.pa165.languageschoolweb.model.CourseFormModel;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.DropDownChoice;
@@ -30,13 +14,11 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.validation.validator.RangeValidator;
 
 /**
- *
- * @author kelnar
+ * @author
  */
 public final class AddCourseForm extends Panel {
 	private static final long serialVersionUID = 1L;
@@ -45,16 +27,13 @@ public final class AddCourseForm extends Panel {
 	private CourseDtoAdapter courseDtoAdapter;
 	
 
-	
-	
-	
-
 	public AddCourseForm(String id) {
 		super(id);
 		add(new CourseInputForm("addCourseForm"));
 	}
 	
 	class CourseInputForm extends Form<CourseFormModel>{
+		private static final long serialVersionUID = 1L;
 		
 		private String selectedLanguage;
 		
@@ -71,30 +50,21 @@ public final class AddCourseForm extends Panel {
 					// TODO validation
 			);
 			
-			//add(new TextField<String>("courseLanguage", String.class)
-				//	.setRequired(true)
-					// TODO validation
-			//);
-			
 			List<String> langs = new ArrayList<String>();
 			for (Teacher.Language l : Teacher.Language.values()) {
 				langs.add(l.toString());
 			}
-			
-			selectedLanguage = "AJ";
-			
-		   
-		
-			 add( new DropDownChoice( "courseLanguage", 
-					 new PropertyModel( this, "selectedLanguage" ), 
-					 langs )
-			 );
-		   
-			
+
+			selectedLanguage = Teacher.Language.AJ.toString();
+
+			add( new DropDownChoice<String>( "courseLanguage",
+			         new PropertyModel<String>( this, "selectedLanguage" ),
+			         langs )
+			   );
+
 			add(new Button("addButton"));
 		}
-		
-		
+
 		public String getSelectedLanguage() {
 			return selectedLanguage;
 		}
@@ -102,22 +72,12 @@ public final class AddCourseForm extends Panel {
 		public void setSelectedLanguage(String selectedLanguage) {
 			this.selectedLanguage = selectedLanguage;
 		}
-		
-		
-		
+	
 		@Override
-		protected void onError() {			
-			PageParameters params = new PageParameters();
-			params.set("error", "Chyba v zadavani modelu");
-			setResponsePage(CoursesPage.class,params);
-		}
-		
-		
 		public void onSubmit()
         {
 			CourseFormModel model = (CourseFormModel) getDefaultModelObject();
 
-			PageParameters params = new PageParameters();
 			try{
 				CourseDto cour = new CourseDto();
 				cour.setLevel(model.getCourseLevel());
@@ -125,12 +85,13 @@ public final class AddCourseForm extends Panel {
 				cour.setLanguage(getSelectedLanguage());
 				
 				courseDtoAdapter.createCourse(cour);
-				params.set("message", "Vygenerováno");
+				getSession().error(getString("courseCreated"));
 			}catch(Exception ex){
-				params.set("error", "Chyba generování dat: " + ex);
+				getSession().error(getString("courseNotCreated"));
+				getSession().error(ex);
 			}
 
-			setResponsePage(CoursesPage.class,params);
+			setResponsePage(CoursesPage.class);
         }
 
 	}
