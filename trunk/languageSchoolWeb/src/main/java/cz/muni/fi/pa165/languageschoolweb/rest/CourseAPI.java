229d579dbfdd2eb5e22835d45cba6afd88384880
kelnar.mar@gmail.com <kelnar.mar@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2012-12-09 16:38:46 +0000

    basic for course rest api

---
+++ trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/rest/CourseAPI.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2012 kelnar.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.muni.fi.pa165.languageschoolweb.rest;
+
+import cz.muni.fi.pa165.languageschool.api.adapters.CourseDtoAdapter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+/**
+ *
+ * @author kelnar
+ */
+
+public class CourseAPI extends HttpServlet {
+
+	@Autowired
+    private CourseDtoAdapter courses;
+	
+	private JsonTranslator translator = new JsonTranslator();
+	
+	/**
+	 * Handles the HTTP
+	 * <code>GET</code> method.
+	 *
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String pathInfo = request.getPathInfo();
+		
+		out.println(pathInfo);
+		
+		if (pathInfo == null) {
+			if (courses == null) {
+				out.println("null 254");
+			} else {
+				out.println( translator.objectToJson(courses.getAllCourses()) );
+			}
+		}
+	}
+
+	/**
+	 * Handles the HTTP
+	 * <code>POST</code> method.
+	 *
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	
+	@Override
+    public void init(ServletConfig config) throws ServletException {
+        SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, config.getServletContext());
+        super.init(config);
+    }
+	
+}
