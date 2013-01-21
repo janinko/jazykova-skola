69ff4ed84260cd74dd226761f1dae7a7fe010dc0
janinko.g@gmail.com <janinko.g@gmail.com@584c94bc-b73d-4272-33e5-607be1db018c>
2013-01-21 19:12:01 +0000

    partialy implement authentization authorization in frontend

--- trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/WicketApplication.java
+++ trunk/languageSchoolWeb/src/main/java/cz/muni/fi/pa165/languageschoolweb/WicketApplication.java
@@ -1,10 +1,15 @@
 package cz.muni.fi.pa165.languageschoolweb;
 
+import cz.muni.fi.pa165.languageschoolweb.security.SpringAuthenticatedWebSession;
+import org.apache.wicket.Session;
+import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
+import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Response;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
-public class WicketApplication extends WebApplication
+public class WicketApplication extends AuthenticatedWebApplication
 {    	
 	@Override
 	public Class<? extends WebPage> getHomePage()
@@ -27,6 +32,22 @@ public class WicketApplication extends WebApplication
         mountPage("/account", AccountPage.class);
         mountPage("/action", ActionPage.class);
         mountPage("/error", ErrorPage.class);
+        mountPage("/login", LoginPage.class);
         mountPage("/404", Error404Page.class);
+	}
+
+	@Override
+	protected Class<? extends AbstractAuthenticatedWebSession> getWebSessionClass() {
+		return SpringAuthenticatedWebSession.class;
+	}
+
+	@Override
+	protected Class<? extends WebPage> getSignInPageClass() {
+		return LoginPage.class;
+	}
+
+	@Override
+	public Session newSession(Request request, Response response) {
+		return new SpringAuthenticatedWebSession(request);
 	}
 }
